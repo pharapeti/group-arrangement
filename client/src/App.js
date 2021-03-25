c2f7150b993734f0a751aaa4fbec0337bfdb54e5
pharapeti <patrice.harapeti@gmail.com>
2021-03-25 23:23:24 +1100

    A bit of state management

--- client/src/App.js
+++ client/src/App.js
@@ -7,30 +7,16 @@ class App extends Component {
     response: '',
     post: '',
     responseToPost: '',
+    userList: []
   };
+
   componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
+    fetch('http://localhost:6060/api/users')
+      .then(response =>
+        response.json()
+          .then((j) => this.setState({ userList: j.map(user => user['first_name']) })))
   }
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    return body;
-  };
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/api/world', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ post: this.state.post }),
-    });
-    const body = await response.text();
-    this.setState({ responseToPost: body });
-  };
+
 render() {
     return (
       <div className="App">
@@ -48,19 +34,12 @@ render() {
             Learn React
           </a>
         </header>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
+        <div>
+          {this.state.userList.map((user, index) => (
+            <p key={index}>{user}</p>
+          ))}
+        </div>
+        <p>Number of users: {this.state.userList.length}</p>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 6060;
 const db_path = process.env.MYSQL_DB || './db/group_arrangement_development.db';
 app.use(express.json()); //Used to parse JSON bodies
 app.use(express.urlencoded()); //Parse URL-encoded bodies
+app.use(cors()); // https://stackoverflow.com/a/63547498/8186540
 
 let db = new sqlite3.Database(db_path, sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
-    console.log('here');
+    console.log('Unable to connect to the database');
     throw new Error(err.message);
   }
   console.log('Connected to the group_arrangement_development database.');
@@ -19,7 +21,6 @@ app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
 app.post('/api/world', (req, res) => {
-  console.log(req.body);
   res.send(
     `I received your POST request. This is what you sent me: ${req.body.post}`,
   );
@@ -27,9 +28,9 @@ app.post('/api/world', (req, res) => {
 
 // Test route which queries all users and returns all the data via JSON
 app.get('/api/users', (req, res) => {
-  console.log('Request for /api/users');
-  db.each(`select * from users`, (err, rows) => {
+  db.all(`select * from users`, (err, rows) => {
     if (err) {
+      console.log(err.message);
       res.send(err);
     } else {
       res.send(rows);
@@ -38,7 +39,7 @@ app.get('/api/users', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.send({ something: 'Hello my friend' })
+  res.send({ something: 'You hit the all other routes matcher' })
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
