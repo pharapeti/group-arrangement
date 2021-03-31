48244f72e4f4226a810a7555dc49a76c4758a7db
pharapeti <patrice.harapeti@gmail.com>
2021-03-31 16:54:00 +1100

    fix db and role names

--- scripts/database_setup.sh
+++ scripts/database_setup.sh
@@ -3,8 +3,8 @@ echo "Starting database setup script"
 echo "NOTICE: Ensure a postgresql server is running and that you have psql installed"
 
 # Create role with superuser privileges
-sudo -u postgres psql -c "CREATE USER group arrangement WITH PASSWORD 'testpassword';"
-sudo -u postgres psql -c "ALTER USER group arrangement WITH SUPERUSER;"
+sudo -u postgres psql -c "CREATE USER group_arrangement WITH PASSWORD 'testpassword';"
+sudo -u postgres psql -c "ALTER USER group_arrangement WITH SUPERUSER;"
 
 # Create database
-sudo -u postgres psql -c "CREATE DATABASE group arrangement_development;"
+sudo -u postgres psql -c "CREATE DATABASE group_arrangement_development;"
