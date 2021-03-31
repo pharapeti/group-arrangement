47f5434dc0d3fe5bf6e21994b16fcdc6110b160c
pharapeti <patrice.harapeti@gmail.com>
2021-03-31 16:11:00 +1100

    fix dependencies

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 
 const app = express();
@@ -8,14 +7,6 @@ const db_path = process.env.MYSQL_DB || './db/group_arrangement_development.db';
 app.use(express.json()); //Used to parse JSON bodies
 app.use(express.urlencoded()); //Parse URL-encoded bodies
 app.use(cors()); // https://stackoverflow.com/a/63547498/8186540
-
-let db = new sqlite3.Database(db_path, sqlite3.OPEN_READWRITE, (err) => {
-  if (err) {
-    console.log('Unable to connect to the database');
-    throw new Error(err.message);
-  }
-  console.log('Connected to the group_arrangement_development database.');
-});
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
@@ -28,14 +19,7 @@ app.post('/api/world', (req, res) => {
 
 // Test route which queries all users and returns all the data via JSON
 app.get('/api/users', (req, res) => {
-  db.all(`select * from users`, (err, rows) => {
-    if (err) {
-      console.log(err.message);
-      res.send(err);
-    } else {
-      res.send(rows);
-    }
-  });
+  res.send('bruh');
 });
 
 app.get('*', (req, res) => {
