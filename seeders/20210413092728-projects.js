2c461c39810e97832cdedf0fed68ae92133ad3c5
pharapeti <patrice.harapeti@gmail.com>
2021-04-13 21:30:28 +1000

    create and associate records

--- seeders/20210413092728-projects.js
+++ seeders/20210413092728-projects.js
@@ -1,47 +1,44 @@
 "use strict";
 
 const { default: userEvent } = require("@testing-library/user-event");
-const sequelize = require("sequelize");
+const serialize = require("sequelize");
+const model = require('../models/index');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert("users", [
-      {
-        user_type: 2,
-        first_name: "Admo",
-        last_name: "Adminyan",
-        external_id: "external_admin",
-        encrypted_password: "$321!pass!123$",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]);
+    const project_owner = await model.User.create({
+      user_type: 2,
+      first_name: "Admo",
+      last_name: "Adminyan",
+      external_id: "external_admin",
+      encrypted_password: "$321!pass!123$",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
 
-    console.log(sequelize);
-    console.log(Sequelize);
-    console.log(sequelize.Model);
+    await model.Project.create({
+      name: "Next Facebook",
+      max_group_size: 6,
+      created_by: project_owner.id,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
 
-    const user = await sequelize.Model.User.findOne({
-      where: { user_type: 2 },
-    });
+    await model.Project.create({
+      name: "Next Amazon",
+      max_group_size: 6,
+      created_by: project_owner.id,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
 
-    return await queryInterface.bulkInsert("projects", [
-      {
-        name: "Next Facebook",
-        max_group_size: 6,
-        created_by: user,
-      },
-      {
-        name: "Next Amazon",
-        max_group_size: 6,
-        created_by: user,
-      },
-      {
-        name: "Next NASA",
-        max_group_size: 6,
-        created_by: user,
-      },
-    ]);
+    await model.Project.create({
+      name: "Next NASA",
+      max_group_size: 6,
+      created_by: project_owner.id,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
