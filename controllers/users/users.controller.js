33fbb838cc0b2003f9cc93784b7816b2ea8d6c12
pharapeti <patrice.harapeti@gmail.com>
2021-04-14 18:00:13 +1000

    Implement simple user authentication Create authentication middleware to return 401 Unauthorized when not logged in Create users/projects router as nested router in the user router

--- controllers/users/users.controller.js
+++ controllers/users/users.controller.js
@@ -26,3 +26,28 @@ exports.findOne = (req, res) => {
       res.status(500).send({ message: 'Server crapped itself' });
     })
 }
+
+exports.login = (request, response) => {
+	var external_id = request.body.external_id;
+	var password = request.body.encrypted_password;
+
+	if (external_id && password) {
+    model.User.findOne({ where: { external_id: external_id,  encrypted_password: password } })
+      .then(_user => {
+        request.session.signed_in = true;
+        request.session.external_id = external_id;
+        response.status(200).send({ message: 'Logged in' })
+      })
+      .catch(_err => {
+        response.send('Incorrect ID and/or Password!');
+      })
+  } else {
+    response.status(400).send({ message: 'External user id and password was not provided' })
+  }
+};
+
+exports.logout = (request, response) => {
+  request.session.signed_in = false;
+  request.session.external_id = null;
+  response.status(200).send({ message: 'Logged out!' })
+};
---
+++ middleware/auth.js
@@ -0,0 +1,9 @@
+module.exports = (request, response, next) => {
+  // If the user is signed in, then continue with the request
+  // If they aren't signed in, response with Unauthorized(401)
+  if (request.session.signed_in) {
+		next();
+	} else {
+		response.status(401).send('Sorry, you do not have the permission to do that');
+	}
+};
