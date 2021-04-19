c9f0f7b4a38ec648a30543eacb5eb8b47e8ecb5f
Yunwei-Zhang <62420557+Yunwei-Zhang@users.noreply.github.com>
2021-04-19 23:30:10 +0800

    v1.0
    basic code

--- client/src/Root.js
+++ client/src/Root.js
@@ -5,6 +5,9 @@ import StudentProfile from './Components/StudentProfile.js'
 import StudentNotification from './Components/StudentNotification.js'
 import StudentProject from './Components/StudentProject.js'
 import StudentGroup from './Components/StudentGroup.js'
+import AdminMainMenu from './Components/AdminMainMenu'
+import AdminNotification from './Components/AdminNotification.js'
+import AdminCreateProject from './Components/AdminCreateProject.js'
 import App from './App'
 
 //here to set all the urls 
@@ -14,12 +17,15 @@ class UrlSet extends Component{
         return(
           <Router>    
             <div>
-             <Route path="/" exact strict component={App}/>     
+              <Route path="/" exact strict component={App}/>     
               <Route path="/student/home" exact strict component={StudentMainMenu}/>     
               <Route path="/student/profile" exact strict component={StudentProfile}/>   
               <Route path="/student/notification" exact strict component={StudentNotification}/> 
               <Route path="/student/project" exact strict component={StudentProject}/>   
               <Route path="/student/project/group" exact strict component={StudentGroup}/>  
+              <Route path="/admin/home" exact strict component={AdminMainMenu}/>  
+              <Route path="/admin/notification" exact strict component={AdminNotification}/> 
+              <Route path="/admin/project/create" exact strict component={AdminCreateProject}/> 
             </div>
           </Router>
         )
