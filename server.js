cff408a223329522c8709053930ef4860b222ada
pharapeti <patrice.harapeti@gmail.com>
2021-04-26 22:04:19 +1000

    Persist session data to database

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const session = require('express-session');
 const cors = require('cors');
 var cookieParser = require('cookie-parser');
 
+// Sequelize ORM
+console.log(process.env.development);
+const Sequelize = require("sequelize");
+
+// Typically would be pulled from environment variables
+// Harcoding this connection string this will only be used for local demo's
+const sequelize = new Sequelize('postgres://group_arrangement:testpassword@localhost:5432/group_arrangement_development');
+var SequelizeStore = require("connect-session-sequelize")(session.Store);
+const store = new SequelizeStore({ db: sequelize });
+
 const app = express();
 const port = process.env.PORT || 6060;
 
@@ -10,7 +20,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors()); // https://stackoverflow.com/a/63547498/8186540
 app.use(cookieParser());
-app.use(session({ secret: 'someSecret', saveUninitialized : true, resave : true }));
+app.use(session({
+  secret: 'someSecret',
+  store,
+  saveUninitialized : true,
+  resave : true
+}));
+
+// Keep session store up to date
+store.sync();
 
 // Admin routes
 require('./routes/admin/projects/projects.routes')(app);
