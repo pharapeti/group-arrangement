916f3a65409f4179b17b8f8e775cd88b1b12612f
pharapeti <patrice.harapeti@gmail.com>
2021-04-27 23:19:56 +1000

    Simple authentication from front-end

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 module.exports = (request, response, next) => {
   // If the user is signed in, then continue with the request
   // If they aren't signed in, response with Unauthorized(401)
-  if (request.session.signed_in && request.session.external_id) {
+  if (request.session.external_id) {
 		next();
 	} else {
 		response.status(401).send('Sorry, you do not have the permission to do that');
