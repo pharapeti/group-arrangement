17449a1283943356deb30ee251d28cecf5fd328c
pharapeti <patrice.harapeti@gmail.com>
2021-04-27 22:15:58 +1000

    Make Student/Groups actions relevant to logged in user

--- controllers/student/groups/groups.controller.js
+++ controllers/student/groups/groups.controller.js
@@ -2,29 +2,52 @@ const model = require('../../../models/index')
 
 // Return all groups associated to the logged in user
 exports.findAll = (req, res) => {
-  model.Group.findAll()
-    .then(groups => {
-      res.send(groups);
+  model.User.findOne({ where: { external_id: req.session.external_id } })
+    .then(user => {
+      const userID = user.id;
+
+      model.GroupAllocation.findAll({ where: { user_id: userID } })
+        .then(group_allocations => {
+          const groupIds = group_allocations.map(group_alloc => { return group_alloc.group_id; })
+
+          model.Group.findAll({ where: { id: groupIds } })
+            .then(groups => {
+              res.send(groups);
+            })
+            .catch(err => {
+              res.status(500).send({ message: err.message || "Some error occurred while retrieving Groups." });
+            })
+        })
+        .catch(err => {
+          res.status(500).send({ message: err.message || "Some error occurred while retrieving Groups." });
+        })
     })
     .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving groups."
-      });
-    });
+      res.status(500).send({ message: err.message || "Some error occurred while retrieving Groups." });
+    })
 }
 
 // Return information about a single group that is associated to the logged in user
 exports.findOne = (req, res) => {
-  model.Group.findByPk(req.params.id)
-  .then(group => {
-    if (group == null) {
-      res.status(404).send({ message: `Group with id: ${req.params.id} does not exist` });
-    } else {
-      res.send(group);
-    }
-  })
-  .catch(err => {
-    res.status(500).send({ message: err.message || 'Something went wrong' });
-  })
+  model.User.findOne({ where: { external_id: req.session.external_id } })
+    .then(user => {
+      const userID = user.id;
+
+      model.GroupAllocation.findAll({ where: { user_id: userID } })
+        .then(group_allocations => {
+          const groupIds = group_allocations.map(group_alloc => { return group_alloc.group_id; })
+
+          if (groupIds && groupIds.find(id => { return id === parseInt(req.params.id)} )) {
+            model.Group.findOne({ where: { id: req.params.id }}).then(group => { res.send(group) });
+          } else {
+            res.status(404).send({ message: `You do not have access to Group with id: ${req.params.id}` });
+          }
+        })
+        .catch(err => {
+          res.status(500).send({ message: err.message || "Some error occurred while retrieving Groups." });
+        })
+    })
+    .catch(err => {
+      res.status(500).send({ message: err.message || "Some error occurred while retrieving Groups." });
+    })
 }
