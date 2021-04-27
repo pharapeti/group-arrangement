72e5ee0fca4a42a5b481fb1e64583f4cb560e7a4
pharapeti <patrice.harapeti@gmail.com>
2021-04-27 21:55:18 +1000

    Student/Project #findOne

--- controllers/student/projects/projects.controller.js
+++ controllers/student/projects/projects.controller.js
@@ -31,5 +31,33 @@ exports.findAll = (req, res) => {
 
 // Return information about a single project
 exports.findOne = (req, res) => {
-  res.send([]);
+  model.User.findOne({ where: { external_id: req.session.external_id } })
+    .then(user => {
+      const userID = user.id;
+
+      model.GroupAllocation.findAll({ where: { user_id: userID } })
+        .then(group_allocations => {
+          const groupIds = group_allocations.map(group_alloc => { return group_alloc.group_id; });
+
+          model.Group.findAll({ where: { id: groupIds } })
+            .then(groups => {
+              const projectIds = groups.map(group => { return group.project_id });
+
+              if (projectIds && projectIds.find(id => { return id === parseInt(req.params.id)} )) {
+                model.Project.findOne({ where: { id: req.params.id }}).then(project => { res.send(project) });
+              } else {
+                res.status(404).send({ message: `You do not have access to Project with id: ${req.params.id}` });
+              }
+            })
+            .catch(err => {
+              res.status(500).send({ message: err.message || 'Something went wrong'});
+            })
+        })
+        .catch(err => {
+          res.status(500).send({ message: err.message || 'Something went wrong'});
+        })
+      })
+      .catch(err => {
+        res.status(500).send({ message: err.message || 'Something went wrong'});
+      })
 }
