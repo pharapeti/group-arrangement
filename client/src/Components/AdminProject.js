c98177520c93d2e131057ac5cb462c5279f58977
Yunwei-Zhang <62420557+Yunwei-Zhang@users.noreply.github.com>
2021-05-19 01:09:00 +0800

    clear page

--- client/src/Components/AdminProject.js
+++ client/src/Components/AdminProject.js
@@ -17,7 +17,7 @@ class AdminProject extends Component{
     }
 
     componentDidMount(){
-        fetch('http://localhost:6060/api/admin/projects/' + this.props.match.params.project_id, {
+        fetch('http://localhost:6060/api/admin/projects/', {
             credentials: 'include',
             headers: { 'Content-Type': 'application/json' }
         })
@@ -48,17 +48,6 @@ class AdminProject extends Component{
             this.setState({ project_groups:j })
             //console.log(this.state.project_groups)
         })
-
-        fetch('http://localhost:6060/api/admin/projects/' + this.props.match.params.project_id + '/project_allocations', {
-            credentials: 'include',
-            headers: { 'Content-Type': 'application/json' },
-        })
-        .then(response => response.json())
-        .then(j => {
-    
-            this.setState({ students_in_projects:j })
-            console.log(this.state.students_in_projects)
-        })
         
     }
 
@@ -85,25 +74,6 @@ class AdminProject extends Component{
         this.props.history.push('/admin/projects/'+ project_id +'/edit')
     }
     
-    //NEED TO FIX
-    handleDelete()
-    {
-        if(window.confirm("Are you sure?"))
-        {
-          window.location.href="/admin/home"
-          fetch('http://localhost:6060/api/admin/projects/' + this.props.match.params.project_id , {
-              method: 'DELETE',
-              headers: { 'Content-Type': 'application/json',
-              'Access-Control-Allow-Origin': "*",
-              'Access-Control-Allow-Methods': "DELETE"},
-              credentials: 'include',
-          })
-          .then(response => response.json())
-          .then(j => {
-            console.log(j)
-          })
-        }
-    }
     
     render() {
         return(
@@ -127,14 +97,7 @@ class AdminProject extends Component{
                 <div className={css.projectrightcontent}>
                     <p className={css.subtitle}>
                         <br/>
-                        Student List:
-                        <br/>
-                        {this.state.students_in_projects.map(students_in_project =>( 
-                            <li className={css.textcontent} key={students_in_project.id}>
-                                {students_in_project.first_name} {students_in_project.last_name}
-                                <br/><br/>
-                            </li>
-                        ))}                 
+                                        
                         <button className={css.addstudentbtn} onClick={()=>this.goToAddStudentPage()}>Add Student</button> 
                         <button className={css.projecttwobutton} onClick={()=>this.goToCreateGroupPage()}>Create Groups</button>
                         <button className={css.projecttwobutton} onClick={()=>this.goToEditGroupPage()}>Edit Groups</button>
@@ -144,18 +107,14 @@ class AdminProject extends Component{
                 <div >
                     <h1 className={css.title}>
                         Project {this.props.match.params.project_id}
-                        <button className={css.projectgraybutton} onClick={()=>this.goToSettingsPage()}>Edit Setting</button>
-                        <button className={css.projectgraybutton} onClick={()=>this.handleDelete()}>Delete</button>
                     </h1> 
                     <p className={css.textcontent}><br/>&nbsp;&nbsp;&nbsp;Description: {this.state.project_name}</p>
                     <p className={css.textcontent}><br/>&nbsp;&nbsp;&nbsp;Max Group Size: {this.state.project_group_size}</p>
-                    <p className={css.textcontent}><br/>&nbsp;&nbsp;&nbsp;Interest:</p>
-                    <p className={css.textcontent}><br/>&nbsp;&nbsp;&nbsp;Skill:</p>
                     <br/>
                     <p className={css.textcontent} style={{fontSize: "35px"}}>&nbsp;&nbsp;Groups:</p>
                       {this.state.project_groups.map(project_group =>( 
                             <li className={css.textcontent} key={project_group.id}>
-                                Group Id: {project_group.id}&nbsp;
+                                Id: {project_group.id}&nbsp;
                                 Group Number: {project_group.group_number}
                                 <br/><br/>
                             </li>
